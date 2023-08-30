17c9ad7c3af8749a22ea4bbcc3c7e5b1042356be
masterbatcoderman10 <suhaib.athar@gmail.com>
2023-08-30 20:18:40 +0400

    deleted useless stuff

--- .firebase/blueeyes-dd387/functions/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.fdfc193c.js","_app/immutable/chunks/scheduler.fb34aa51.js","_app/immutable/chunks/index.1d67b316.js","_app/immutable/chunks/singletons.7f6c92c6.js"];
-export const stylesheets = [];
-export const fonts = [];
--- .firebase/blueeyes-dd387/functions/nodes/2.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 2;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.21b66b02.js","_app/immutable/chunks/scheduler.fb34aa51.js","_app/immutable/chunks/index.1d67b316.js"];
-export const stylesheets = ["_app/immutable/assets/2.0883a225.css"];
-export const fonts = [];
